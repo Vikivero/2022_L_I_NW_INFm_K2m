57dff450fae10ac9c2500c64e62c90fdc2b4c9fb
vikivero <vikichan.ch@gmail.com>
2022-11-13 15:16:18 +0100

    Poprowka

--- test/test_views.py
+++ test/test_views.py
@@ -15,4 +15,4 @@ class FlaskrTestCase(unittest.TestCase):
 
     def test_msg_with_output(self):
         rv = self.app.get('/?output=json')
-        self.assertEqual(b'{ "imie":"Viktoriia", "msgs":"Hello World!"}', rv.data)  # noqa: E501
+        self.assertEqual(b'{ "imie":"Viktoriia", "msg":"Hello World!"}', rv.data)  # noqa: E501
